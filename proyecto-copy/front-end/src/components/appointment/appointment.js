7a1072b4f778f3ccb3676db0f2bad88a0e16268d
fiorod <fiorella.rodriguez8@ulatina.net>
2024-04-18 13:20:27 -0600

    Citas actualizado
    Actualizacion

--- proyecto-copy/front-end/src/components/appointment/appointment.js
+++ proyecto-copy/front-end/src/components/appointment/appointment.js
@@ -10,72 +10,96 @@ import Autocomplete from '@mui/material/Autocomplete';
 
 
 
-const options = ['Dr. Omar Alfaro', 'Dra. Alejandra Ramos','Dra. Fabiana Aguilar', 'Dr. Antonio Porras' ];
+const options = ['Dr. Omar Alfaro', 'Dra. Alejandra Ramos', 'Dra. Fabiana Aguilar', 'Dr. Antonio Porras'];
 
- function ControllableStates() {
-  const [value, setValue] = React.useState(options[0]);
+function ControllableStates() {
+    const [value, setValue] = React.useState(options[0]);
 
-  return (
-    <div>
-        <center>
-      <br />
-      <Autocomplete
-        
-       
-        id="controllable-states-demo"
-        options={options}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Seleccion de doctores" />}
-     />
-   
-   </center></div>
-  );
+    return (
+        <div>
+            <center>
+                <br />
+                <Autocomplete
+
+
+                    id="controllable-states-demo"
+                    options={options}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Seleccion de doctores" />}
+                />
+
+            </center></div>
+    );
 }
 
 
 
 function Label({ componentName, valueType, isProOnly }) {
-  const content = (
-    <span>
-     <strong>{componentName}</strong> 
-    </span>
-  );
+    const content = (
+        <span>
+            <strong>{componentName}</strong>
+        </span>
+    );
 
 
 
-  return content;
+    return content;
 }
 
- function CommonlyUsedComponents() {
+function CommonlyUsedComponents() {
     return (
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer
-                    components={[
-                        'DatePicker',
-                        'TimePicker',
-                    ]}
-                >
-                    <DemoItem label={<Label componentName="Seleccione la fecha" valueType="date" />}>
-                        <DatePicker />
-                    </DemoItem>
-                    <DemoItem label={<Label componentName="Seleccione la hora" valueType="time" />}>
-                        <TimePicker />
-                    </DemoItem>
-                </DemoContainer>
-            </LocalizationProvider>
-     
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh'}}>
+            <div >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer
+                        components={[
+                            'DatePicker',
+                            'TimePicker',
+                        ]}
+                    >
+                        <DemoItem label={<Label componentName="Seleccione la fecha" valueType="date" />}>
+                            <DatePicker />
+                        </DemoItem>
+                        <DemoItem label={<Label componentName="Seleccione la hora" valueType="time" />}>
+                            <TimePicker />
+                        </DemoItem>
+                    </DemoContainer>
+                </LocalizationProvider>
+            </div>
+        </div>
     );
 }
 
-    
- 
- 
-export default function Appointment() {
+const options1 = ['Examen de vista', 'Consulta para cirugia', 'Consulta general', 'Otro'];
+
+function ControllableStates1() {
+    const [value, setValue] = React.useState(options1[0]);
+
+    return (
+        <div>
+            <center>
+                <br />
+                <Autocomplete
+
+
+                    id="controllable-states-demo"
+                    options={options1}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Seleccion de servicios" />}
+                />
+
+            </center></div>
+    );
+}
+
+
+    export default function Appointment() {
   return (
     <div className="appointment">
-      <h1>Agendar Cita</h1>
-      <ControllableStates/>
-      <CommonlyUsedComponents/>
+        <h1>Agendar Cita</h1>
+        <ControllableStates />
+        <CommonlyUsedComponents />
+        <ControllableStates1 />
     </div>
-  );
+    );
 }
